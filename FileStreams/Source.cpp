dd101f4c896e3639dcb8759286ce7d270b864c4b
Prab <prab.missier@gmail.com>
2022-10-24 23:08:36 -0700

    File Stream code

---
+++ FileStreams/Source.cpp
@@ -0,0 +1,41 @@
+//File Operations
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+
+
+int main()
+{
+	
+	//Reading from a file
+	ifstream myInputFile("C:/CPP/professions.txt");		
+
+	//ifstream also inherits from istream
+	while (myInputFile)
+	{
+		string aline;
+		getline(myInputFile, aline);
+		cout << aline << endl;
+	}
+
+	myInputFile.close();
+		
+	//Writing to a file
+	//ofstream also inherits from ostream
+	ofstream myOutputFile("C:/CPP/professions.txt", ios::app);
+	cout << "Enter a profession : ";
+	string aprofession;
+	while (getline(cin, aprofession)) {
+
+		myOutputFile << "\n"<<aprofession;
+	}	
+
+	myOutputFile.close();
+	
+	return 0;
+}
