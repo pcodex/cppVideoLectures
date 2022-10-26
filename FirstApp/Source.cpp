aa84d231ed1930e43e6f05b6d9268fd7967cbcc8
Prab <prab.missier@gmail.com>
2022-10-25 19:14:20 -0700

    Updated FirstApp used for stdin and stdout

--- FirstApp/Source.cpp
+++ FirstApp/Source.cpp
@@ -1,16 +1,16 @@
+//Initial program to demonstrate stdin and stdout
 #include <iostream>
 #include <string>
 
 int main()
 {
-	std::cout << "Hello World\n"; 
+	std::cout << "Hello World\n"; //cout is an object of the class ostream
 
-	std::cout << "Enter your name :"; //ostream
-	
+	std::cout << "\nEnter your name :";
 	std::string aname;
-	
-	std::cin >> aname; //istream
-	std::cout << "\nYou entered : " << aname;
+
+	std::cin >> aname; //cin is an object of the class istream
+	std::cout << "\nThe name that you entered is :" << aname;
 
 	return 0;
 }
